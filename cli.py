b57408f14a98d97483dcc441c5692b50de74284f
Jonatandb <jonatandb@gmail.com>
2023-06-23 19:27:12 -0300

    users command added to list users

--- cli.py
+++ cli.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+import click
+import json_manager
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+def users():
+    data = json_manager.read_json()
+    print(data)
+
+if __name__ == '__main__':
+    cli()
