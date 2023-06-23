0550cf380f256f780536d27611ed19511d256a25
Jonatandb <jonatandb@gmail.com>
2023-06-23 19:36:13 -0300

    Improving user listing

--- cli.py
+++ cli.py
@@ -8,9 +8,13 @@ def cli():
 @cli.command()
 def users():
     users = json_manager.read_json()
-    print('Id\tName\t\tLastname')
-    for user in users:
-        print( f"{user['id']}\t{user['name']}\t{user['lastname']}")
+    if(users):
+      print('Id\tName\t\tLastname')
+      for user in users:
+          print( f"{user['id']}\t{user['name']}\t{user['lastname']}")
+    else:
+      print('No users')
+
 
 if __name__ == '__main__':
     cli()
